af9e573da21cd0b7cd2cd59e5d0e9f0818b188db
Amitkumar07Sharma <amitkumarsharma3577@gmail.com>
2023-11-10 21:20:13 +0530

    added

--- Script1.js
+++ Script1.js
@@ -1,4 +1,9 @@
 let a = "Amitkumar sharma"
-let b = ["Javascript","python", "Cypress"]
+let b = ["Javascript", "python", "Cypress"]
+const c = {
+    firstname: "Kanika Sharma",
+    locationw: "Pune"
+}
 console.log(a)
 console.log(b)
+console.log(c)
